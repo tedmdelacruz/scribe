ddbf0a664138f351e0f237811c69255c5d4eda9b
Ted Mathew dela Cruz <tedmdelacruz@gmail.com>
2016-05-31 20:17:57 +0800

    Layout tweaks in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-body">
                     <h1>Scribe</h1>

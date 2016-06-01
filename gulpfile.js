772f4d49c421009962cb705ce7b65b689b2fcf84
Ted Mathew dela Cruz <tedmdelacruz@gmail.com>
2016-06-01 17:13:18 +0800

    Add local jQuery and Bootstrap assets

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,8 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.sass('app.scss')
+        .copy('bower_components/bootstrap/dist/css/bootstrap.min.css', 'public/css/vendor/')
+        .copy('bower_components/jquery/dist/jquery.min.js', 'public/js/vendor/')
+        .copy('bower_components/bootstrap/dist/js/bootstrap.min.js', 'public/js/vendor/')
         .browserify('write/index.js', 'public/js/write.js');
 });
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link href="{{ asset('css/vendor/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body id="app-layout">
@@ -50,8 +50,8 @@
     @yield('content')
 
     <!-- JavaScripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script src={{ asset('js/vendor/jquery.min.js') }}></script>
+    <script src={{ asset('js/vendor/bootstrap.min.js') }}></script>
 
     @yield('scripts')
 

de6a157390c3e4628470cc74d93c397d7483ed59
Ted Mathew dela Cruz <tedmdelacruz@gmail.com>
2016-06-03 20:50:17 +0800

    Add a more tasteful Welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
+<div class="jumbotron">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Scribe</h1>
+                <hr>
+                <p>A simple Markdown writer with real-time preview</p>
+                <p><a href="{{ url('/write') }}" class="btn btn-default">Write your first entry now</a></p>
+            </div>
         </div>
     </div>
 </div>
